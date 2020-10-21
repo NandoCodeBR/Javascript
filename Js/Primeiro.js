4b931bbdee8aaaaf28c59c1da8d4542ce5b2f097
Fernando <fernando.muniz@outlook.com.br>
2020-10-21 17:22:46 -0300

    Exercícios

---
+++ Js/Primeiro.js
@@ -0,0 +1,5 @@
+window.alert("OLÁ MUNDO!");
+var n1 = window.prompt("digite o primeiro numero");
+var n2 = window.prompt("digite o segundo numero");
+var soma = n1 + n2;
+document.write('${soma.toUpperCase()} seu nome tem ${soma.lenght} letras');
